2826020b0b0f69f13de984b306f17e8d0ad7938d
= <ganna.andrushko@gmail.com>
2023-09-18 17:07:46 +0800

    removeProduct from cart && add badge for cart

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Cart.css';
-
+import {PiTrashThin} from'react-icons/pi';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectCartItems } from '../../features/language/cartSlice';
 import { addToCart, removeFromCart } from '../../features/language/cartSlice';
@@ -48,38 +48,50 @@ const Cart: React.FC<CartProps> = () => {
   return (
     <div className="container_cart">
       <div className='cart_items_wrapper'>
-      {productsInCart.map((product: any) => (
-        <div key={product.id} className="cart_item">
-          <img src={product.imgSrc} width={60} alt="" />
-          <div>
-            <h3>{product.title}</h3>
-            <h4>{product.selectedSize}</h4>
-            <h4>{product.price}</h4>
-          </div>
-          <label htmlFor={`quantity-${product.id}`}></label>
-          <select
-            className="cart-dropdown-select"
-            id={`quantity-${product.id}`}
-            value={selectedQuantities[product.id] || 0}
-            onChange={(e) =>
-              handleQuantityChange(product.id, parseInt(e.target.value))
-            }
-          >
-            <option value={1} className="cart-dropdown-option">
-              1
-            </option>
-            <option value={2} className="cart-dropdown-option">
-              2
-            </option>
-            <option value={3} className="cart-dropdown-option">
-              3
-            </option>
-          </select>
-        </div>
-      ))}
+        {productsInCart.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          productsInCart.map((product: any) => (
+            <div key={product.id} className="cart_item">
+              <img src={product.imgSrc} width={60} alt="" />
+              <div>
+                <h3>{product.title}</h3>
+                <h4>{product.selectedSize}</h4>
+                <h4>{product.price}</h4>
+              </div>
+              <label htmlFor={`quantity-${product.id}`}></label>
+              <select
+                className="cart-dropdown-select"
+                id={`quantity-${product.id}`}
+                value={selectedQuantities[product.id] || 0}
+                onChange={(e) =>
+                  handleQuantityChange(product.id, parseInt(e.target.value))
+                }
+              >
+                <option value={0} className="cart-dropdown-option">
+                  0
+                </option>
+                <option value={1} className="cart-dropdown-option">
+                  1
+                </option>
+                <option value={2} className="cart-dropdown-option">
+                  2
+                </option>
+                <option value={3} className="cart-dropdown-option">
+                  3
+                </option>
+              </select>
+              <PiTrashThin
+                size={32}
+                className='icon_cart_remove'
+                onClick={() => dispatch(removeFromCart(product.id))}
+              />
+            </div>
+          ))
+        )}
       </div>
       <div className="cart_right">
-       <h1> Payment Summary</h1>
+        <h1>Payment Summary</h1>
         <h3>Total Price: {calculateTotalPrice().toFixed(2)}</h3>
         <h3>Delivery: {deliveryPrice}$</h3>
         <h3>Total: {(calculateTotalPrice() + deliveryPrice).toFixed(2)}</h3>
--- src/pages/header/mainhead/HeadRight.tsx
+++ src/pages/header/mainhead/HeadRight.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import {PiUserCircleGearThin, PiHeartLight, PiShoppingCartSimpleLight} from'react-icons/pi'
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { selectCartItems } from '../../../features/language/cartSlice';
 
 
 function HeadRight() {
-const navigate = useNavigate()
+const navigate = useNavigate();
+const productsAdded = useSelector(selectCartItems);
+
 
 
   return (
     <ul className='header_part_container'>
     <li className="header_actions" onClick={()=> navigate('/signIn')}>
-      <PiUserCircleGearThin  size={32}/>
+      <PiUserCircleGearThin  size={32} className='relatve'/>
     </li>
     <li className="header_actions" onClick={()=> navigate('/cart')}>
       <PiHeartLight size={32} />
+      
     </li>
     <li className="header_actions" onClick={()=> navigate('/cart')} >
-      <PiShoppingCartSimpleLight size={32} />
+      <PiShoppingCartSimpleLight size={32} /><span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    {productsAdded.length}
+  </span>
     </li>
         </ul>
   )
